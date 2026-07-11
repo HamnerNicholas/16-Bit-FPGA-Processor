b48d205632699ea5c4c920a6c164767b9882267e
Nicholas Hamner <nicholashamner63@gmail.com>
2026-07-11 00:00:38 -0700

    Add files via upload

---
+++ rtl/IO/IO_MODULE.v
@@ -0,0 +1,44 @@
+module IO_MODULE #(
+    parameter DATA_WIDTH = 8
+)(
+    input clk,
+    input rst,
+    input halt,
+
+    input ttyLoad,
+    input ttyALoad,
+
+    input [DATA_WIDTH-1:0] accOut,
+    input [DATA_WIDTH-1:0] imm,
+
+    output [6:0] HEX0,
+	 output [DATA_WIDTH-1:0] display_debug,
+    output [6:0] HEX1
+);
+
+    wire cpu_enable = !halt;
+
+    reg [DATA_WIDTH-1:0] display_reg;
+
+    always @(posedge clk) begin
+        if (rst)
+            display_reg <= {DATA_WIDTH{1'b0}};
+        else if (cpu_enable && ttyLoad)
+            display_reg <= imm;
+        else if (cpu_enable && ttyALoad)
+            display_reg <= accOut;
+    end
+	 
+	 assign display_debug = display_reg;
+
+    SEVEN_SEG hex_low (
+        .nibble(display_reg[3:0]),
+        .segments(HEX0)
+    );
+
+    SEVEN_SEG hex_high (
+        .nibble(display_reg[7:4]),
+        .segments(HEX1)
+    );
+
+endmodule
